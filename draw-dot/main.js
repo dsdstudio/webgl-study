f9a2c0ab809046e9f3ad3e4c89b08b7708020ae9
보롱킴 <dsdgun@gmail.com>
2017-01-22 21:32:40 +0900

    [polishing] remove unused shader attribute and varyings

--- draw-dot/main.js
+++ draw-dot/main.js
@@ -2,11 +2,8 @@ let canvas = document.getElementById('glcanvas');
 const gl = BGL.initGL(canvas);
 const vshader = BGL.createShader(
     `attribute vec3 aVertexPosition;
-    attribute vec4 aVertexColor;
-    varying vec4 vColor;
    
     void main(void) {
-        vColor = aVertexColor;
         gl_Position = vec4(aVertexPosition, 1.0);
         // 포인트 스프라이트, 최소 1이상이여한다, gl_PointSize 는 예약어
         gl_PointSize = 5.0;
@@ -14,16 +11,12 @@ const vshader = BGL.createShader(
     `,'vs');
 const fshader = BGL.createShader(
     `precision mediump float;
-    varying vec4 vColor;
-    
     void main(void) {
         gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
     }
     `,'fs');
 const program = BGL.createProgram(vshader, fshader);
-
 program.aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
-program.aVertexColor = gl.getAttribLocation(program, 'aVertexColor');
 
 const vertexBuffer = BGL.createBuffer([
     0.0, 0.5, 0.0,
